de6be3c44d5234562a9843cd584d0a6c217efa82
nkhakwan <post_khan@yahoo.com>
2020-07-10 09:15:07 -0700

    actions file created only for updating master list and generating detailed item details

---
+++ src/actions/index.js
@@ -0,0 +1,35 @@
+export const actionFormTgl =()=> ({
+  type: 'FORM_TGL'
+});
+
+export const actionAdd = (newKeg)=> {
+    const { id, name, brand, img, price, content, quantity } = newKeg;
+    return {
+      type: 'ADD_KEG',
+      id : id,
+      name : name,
+      brand : brand,
+      img : img,
+      price : price,
+      content: content,
+      quantity: quantity
+    }
+  }
+
+  
+
+  export const actionDecQty = (id) => {
+    return{
+      type: 'DEC_QTY',
+      id : id
+    }
+  }
+
+  export const actionDetail = (macroKegList, id) => {
+    return {
+      type: 'DETAIL',
+      id : id,
+      macroKegList : macroKegList
+
+    }
+  }
