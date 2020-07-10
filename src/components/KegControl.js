edd6742a39464ab33eaa874498b85e34c845554d
nkhakwan <post_khan@yahoo.com>
2020-07-10 09:01:09 -0700

    KegControl changes. For the time being on detailed view control and master list is being planned to move to redux. Two reducers with macroKegList and detailItem would be written next

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react'
 import NewKeg from './NewKeg'
 import KegList from './KegList'
 import Details from './Details'
+import * as a from './../actions';
+import { connect } from 'react-redux';
+import PropTypes from "prop-types";
+
 
 export default class KegControl extends Component {
   constructor(props) {
     super(props);
     this.state = {
       formVisibleOnPage: false,
-      masterKegList: [],
       detail: false,
-      detailItem: []
     };
     this.handleClick = this.handleClick.bind(this); 
   }
   
-
+  
+  
 
   handleClick = () => {
     this.setState(lastState => ({
@@ -23,34 +26,37 @@ export default class KegControl extends Component {
     }));
   }
 
+  
+
+  
 
   onNewKegCreation = (newKeg) => {
-    const newMasterKegList = this.state.masterKegList.concat(newKeg);
+    const {dispatch} =this.props;
+    const action1 = a.actionAdd(newKeg);
+    dispatch(action1);
     this.setState({
-      masterKegList: newMasterKegList,
       formVisibleOnPage: false
     });
   }
-
+  
   
   buy = (id) => {
-    this.setState(state => {
-      const masterKegList = state.masterKegList.map(element => {
-        if (element.id === id && element.quantity > 0) {
-          return { ...element, quantity: element.quantity - 1 }
-        } else {
-          return element
-        }
-      });
-      return { masterKegList }
-    })
+    const {dispatch} =this.props;
+    const action = a.actionDecQty(id);
+    dispatch(action);
   }
+
+ 
 
   detail = (id) => {
-    const newItem = this.state.masterKegList.filter(keg => keg.id === id)
-    this.setState({ detail : true, detailItem : newItem });
+    const {dispatch} =this.props;
+    console.log(this.props.masterKegList);
+    const action = a.actionDetail(this.props.masterKegList, id);
+    dispatch(action);
+    this.setState({ detail : true });
   }
 
+  
   back = () =>{
     this.setState({ detail : false });
   }
@@ -59,16 +65,16 @@ export default class KegControl extends Component {
     let currentlyVisibleState = null;
     let buttonText = null;
     if (this.state.detail){
-      console.log(`detailItem value is here ${this.state.detailItem}`);
-      currentlyVisibleState = <Details detailItem={this.state.detailItem[0]} back={this.back } />;
+      currentlyVisibleState = <Details detailItem={this.props.detailItem} back={this.back } />;
       buttonText = "Not to be clicked"
     } else if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewKeg onNewKegCreation={this.onNewKegCreation} />
       buttonText = "cancel"; 
     } else if (!this.state.formVisibleOnPage) {
-      currentlyVisibleState = <KegList kegList={this.state.masterKegList} buy={this.buy} detail={this.detail} />;
+      currentlyVisibleState = <KegList kegList={this.props.masterKegList} buy={this.buy} detail={this.detail} />;
       buttonText = "Add Kegs";
     }
+  
     return (
       <React.Fragment>
         {currentlyVisibleState}
@@ -77,5 +83,32 @@ export default class KegControl extends Component {
     );
   }
 }
+
+
+  
+
+KegControl.propTypes = {
+  masterKegList: PropTypes.object,
+  formVisibleOnPage: PropTypes.bool
+};
+
+const mapStateToProps = state => {
+  return {
+    masterKegList: state.macroKegList,
+    detailItem : state.detailItem
+  }
+}
+
+KegControl = connect(mapStateToProps)(KegControl);
+
+
+
+
+
+
+
+
+
+
 
 
